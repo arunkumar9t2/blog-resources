2c1850698069bff8db9f21e05ffd7fb244d53d8d
Arunkumar <arunk.beece@gmail.com>
2019-05-18 22:33:26 +0800

    Initial commit

---
+++ intent-source-bounds/app/src/main/java/dev/arunkumar/android/CircularRevealLauncherActivity.kt
@@ -0,0 +1,12 @@
+package dev.arunkumar.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CircularRevealLauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_circular_reveal_launcher_activity)
+    }
+}
